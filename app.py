580ada66a28e9c4701795b1b7bf3913f9580ddd2
Văn Quang <vanquang79535@gmail.com>
2025-06-26 12:40:40 +0700

    fix Flask render

--- app.py
+++ app.py
@@ -89,4 +89,6 @@ def download_vcf():
     return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
